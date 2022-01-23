251deaaddd86a9c2839ea476573b62251978a3d6
dsamuel32 <diego.samuel.alves@gmail.com>
2022-01-23 11:57:36 -0300

    Alterando routing key fila de produtos

--- product-command/src/main/java/br/com/estore/productcommand/constants/RabbitMQ.java
+++ product-command/src/main/java/br/com/estore/productcommand/constants/RabbitMQ.java
@@ -6,8 +6,8 @@ public final class RabbitMQ {
 
     public static final String DIRECT_EXCHANGE = PREFIX + "direct.exchange";
 
-    public static final String PRODUCT_ROUTING_KEY = ".received";
+    public static final String PRODUCT_ROUTING_KEY = "product.received";
 
-    public static final String PRODUCT_QUEUE = PREFIX + "product" + PRODUCT_ROUTING_KEY;
+    public static final String PRODUCT_QUEUE = PREFIX + PRODUCT_ROUTING_KEY;
 
 }
